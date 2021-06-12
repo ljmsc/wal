ace3f813341bc23f8a6037127ccc6bce9511e8a1
Lukas Schmitt <git@ljmsc.dev>
2021-06-12 17:33:03 +0200

    fix of by one error and improve internal segment docu

--- segment.go
+++ segment.go
@@ -21,6 +21,10 @@ type segment struct {
 	safe int64
 }
 
+// openSegment opens a existing or new segment file
+// _name is the full filepath of the segment
+// _slit is the amount of blocks which can be written to a single filesystem page
+// _size is the amount of blocks which can be written to the segment file. one block is always reserved for the header
 func openSegment(_name string, _split int64, _size int64) (*segment, error) {
 	s := segment{
 		offsets: []int64{},
@@ -99,6 +103,9 @@ func (s *segment) writeHeader(_split int64, _size int64) error {
 	}
 
 	blksize := ps / _split
+	if blksize < headerLength {
+		return fmt.Errorf("the block size must be greater or equal to the segment header (%d bytes)", headerLength)
+	}
 	// segment file is empty
 	s.header = header{
 		Version: 1,
@@ -125,6 +132,7 @@ func (s *segment) isBlock(_offset int64) bool {
 	return _offset%s.header.Block == 0
 }
 
+// size returns the current segment size in bytes
 func (s *segment) size() (int64, error) {
 	stat, err := s.file.Stat()
 	if err != nil {
@@ -161,7 +169,7 @@ func (s *segment) readPage(_pageData []byte, _offset int64) (int64, error) {
 		}
 	}
 
-	return int64(copy(_pageData, pData[pDelta:n])), nil
+	return int64(copy(_pageData, pData[pDelta:n])), err
 }
 
 // readAt reads the record from _offset
@@ -181,11 +189,14 @@ func (s *segment) readAt(_record *record, _offset int64) error {
 	pData := make([]byte, s.header.Page)
 	n, err := s.readPage(pData, _offset)
 	if err != nil {
-		if errors.Is(err, io.EOF) {
+		if !errors.Is(err, io.EOF) {
+			return err
+		}
+		if n == 0 {
 			return ErrEntryNotFound
 		}
-		return err
 	}
+
 	pData = pData[:n]
 
 	if err := _record.unmarshalMetadata(pData[:recordMetadataLength]); err != nil {
@@ -235,6 +246,7 @@ func (s *segment) readFrom(_offset int64, _n int64) (<-chan envelope, error) {
 		defer close(out)
 
 		var outc int64
+		var eof bool
 		rp := createRecordParser(s.header.Block)
 		nextOffset := _offset
 		var chunk []byte
@@ -247,36 +259,44 @@ func (s *segment) readFrom(_offset int64, _n int64) (<-chan envelope, error) {
 				out <- envelope{err: err}
 				return
 			}
-			if !complete {
-				chunk = make([]byte, s.header.Page)
-				n, err := s.readPage(chunk, nextOffset)
+			if complete {
+				r := record{}
+				parsOffset, err := rp.read(&r)
 				if err != nil {
-					if errors.Is(err, io.EOF) {
-						return
-					}
 					out <- envelope{err: err}
 					return
 				}
-				chunk = chunk[:n]
-				if err := rp.write(chunk); err != nil {
-					out <- envelope{err: err}
+				out <- envelope{
+					offset: parsOffset + _offset,
+					record: r,
+				}
+				if eof {
 					return
 				}
-				nextOffset += n
+				outc++
 				continue
 			}
 
-			r := record{}
-			parsOffset, err := rp.read(&r)
+			chunk = make([]byte, s.header.Page)
+			n, err := s.readPage(chunk, nextOffset)
 			if err != nil {
+				if !errors.Is(err, io.EOF) {
+					out <- envelope{err: err}
+					return
+				}
+				if n == 0 {
+					return
+				}
+				// mark segment as EOF, loop should use the rest of the read data and return
+				eof = true
+			}
+
+			chunk = chunk[:n]
+			if err := rp.write(chunk); err != nil {
 				out <- envelope{err: err}
 				return
 			}
-			out <- envelope{
-				offset: parsOffset + _offset,
-				record: r,
-			}
-			outc++
+			nextOffset += n
 		}
 	}()
 
@@ -356,5 +376,8 @@ func (s *segment) truncate(_offset int64) error {
 
 // close closes the segment file
 func (s *segment) close() error {
-	return s.file.Close()
+	if err := s.file.Close(); err != nil {
+		return fmt.Errorf("can't close segment file: %w", err)
+	}
+	return nil
 }
